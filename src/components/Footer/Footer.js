29882da6dcfedca2687a7801b4d594bfc323f6ef
Kozihodja Koziyon <kozien95@yandex.ru>
2021-06-06 12:31:49 +0400

    Исправить ссылки

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,9 +8,9 @@ import React from "react";
             <div className='footer__navBar'>
                 <p className='footer__year'>&#169; 2020</p>
                 <ul className='footer__links'>
-                    <li><a href='#/' className='footer__link'>Яндекс.Практикум</a></li>
-                    <li><a href='#/' className='footer__link'>GitHub</a></li>
-                    <li><a href='#/' className='footer__link'>Facebook</a></li>
+                    <li><a href='https://praktikum.yandex.ru/profile/web/' className='footer__link' target='blank'>Яндекс.Практикум</a></li>
+                    <li><a href='https://github.com/Kozihodja/' className='footer__link' target='blank'>GitHub</a></li>
+                    <li><a href='https://www.facebook.com/' className='footer__link' target='blank'>Facebook</a></li>
                 </ul>
             </div>
         </section>
--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -8,16 +8,16 @@ import link from '../../../images/link.svg';
             <p className='portfolio__title'>Портфолио</p>
             <ul className='portfolio__lists'>
                 <li className='portfolio__item'>
-                    <a href='#/' className='portfolio__projectName'>Статичный сайт</a>
-                    <a href='#/'><img src={link} className='portfolio__linkImg' alt="Стрелка" /></a>
+                    <a href='https://github.com/Kozihodja/russian-travel' target='blank' className='portfolio__projectName'>Статичный сайт</a>
+                    <a href='https://github.com/Kozihodja/russian-travel' target='blank'><img src={link} className='portfolio__linkImg' alt="Стрелка" /></a>
                 </li>
                 <li className='portfolio__item'>
-                    <a href='#/' className='portfolio__projectName'>Адаптивный сайт</a>
-                    <a href='#/'><img src={link} className='portfolio__linkImg' alt="Стрелка" /></a>
+                    <a href='https://github.com/Kozihodja/russian-travel' target='blank' className='portfolio__projectName'>Адаптивный сайт</a>
+                    <a href='https://github.com/Kozihodja/russian-travel' target='blank'><img src={link} className='portfolio__linkImg' alt="Стрелка" /></a>
                 </li>
                 <li className='portfolio__item'>
-                    <a href='#/' className='portfolio__projectName'>Одностраничное приложение</a>
-                    <a href='#/'><img src={link} className='portfolio__linkImg' alt="Стрелка" /></a>
+                    <a href='https://github.com/Kozihodja/react-mesto-auth' target='blank' className='portfolio__projectName'>Одностраничное приложение</a>
+                    <a href='https://github.com/Kozihodja/react-mesto-auth' target='blank'><img src={link} className='portfolio__linkImg' alt="Стрелка" /></a>
                 </li>
             </ul>
         </div>
